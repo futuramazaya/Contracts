edcf4d5974a7d6d1ba571017b1afe477a2604cc6
grimreaper619 <76556605+grimreaper619@users.noreply.github.com>
2021-06-02 02:25:40 +0530

    Update and rename Green Dildo.sol to Green Lend.sol

--- Green Lend.sol
+++ Green Lend.sol
@@ -436,7 +436,7 @@ contract Ownable is Context {
 
 
 
-contract GreenDildo is Context, IERC20, Ownable {
+contract GreenLend is Context, IERC20, Ownable {
     using SafeMath for uint256;
     using Address for address;
 
@@ -449,15 +449,15 @@ contract GreenDildo is Context, IERC20, Ownable {
     uint256 private constant _max_excluded = 10;
    
     uint256 private constant MAX = ~uint256(0);
-    uint256 private constant _tInitial = 100000000000 * 10**18;
+    uint256 private constant _tInitial = 100 * 10**6 * 10**9;
     uint256 private _rTotal = (MAX - (MAX % _tInitial));
     uint256 private _tFeeTotal;
     uint256 private _tBurned;
     uint256 private _startTime;
 
-    string private _name = 'Green Dildo';
-    string private _symbol = 'DILDO';
-    uint8 private _decimals = 18;
+    string private _name = 'Green Lend';
+    string private _symbol = 'GLEND';
+    uint8 private _decimals = 9;
 
     constructor () {
         _startTime = block.timestamp;
@@ -681,7 +681,7 @@ contract GreenDildo is Context, IERC20, Ownable {
     }
 
     function _getTValues(uint256 tAmount) private pure returns (uint256, uint256, uint256) {
-        uint256 tFeeHalf = tAmount.mul(25).div(1000);
+        uint256 tFeeHalf = tAmount.mul(10).div(1000);
         uint256 tFee = tFeeHalf.mul(2);
         uint256 tTransferAmount = tAmount.sub(tFee);       
         return (tTransferAmount, tFee, tFeeHalf);
